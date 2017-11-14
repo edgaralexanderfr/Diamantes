8ee7c1623e9f4789ebd056911a72f6ca3a2ae1d4
Edgar Alexander Franco <edgaralexanderfr@gmail.com>
2017-11-13 22:29:38 -0400

    Agregadas modalidades Jugador vs Máquina / Jugador1 vs Jugador2 y conteo de puntajes.

--- diamantes.c
+++ diamantes.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <unistd.h>
 #include "diamantes.h"
 
 void jugador () {
@@ -49,11 +50,22 @@ void mostrar_tablero (int tipo_juego) {
 	// Inicializamos el jugador
 	int jugador = (tipo_juego == PC) ? JUGADOR : JUGADOR_1 ;
 	// Empezamos el loop del juego y lo vamos mostrando
-	int f, c, fila, columna;
+	int huecos = 0;
+	int puntaje1 = 0;
+	int puntaje2 = 0;
+	int f, c, fila, columna, casilla_y, casilla_x, numero, by;
 
 	while (1) {
 		// Mostramos el estado
 		cls();
+
+		if (tipo_juego == PC) {
+			printf("Jugador: %i\n", puntaje1);
+			printf("Maquina: %i\n\n", puntaje2);
+		} else {
+			printf("Jugador1: %i\n", puntaje1);
+			printf("Jugador2: %i\n\n", puntaje2);
+		}
 
 		for (y = 0; y <= dimension; y++) {
 			for (x = 0; x <= dimension; x++) {
@@ -70,34 +82,55 @@ void mostrar_tablero (int tipo_juego) {
 			printf("\n");
 		}
 
-		// Pedimos la casilla y procesamos la lógica
-		printf("\nFila: ");
-		scanf("%i", &fila);
-		getchar();
-
-		if (fila == 0) {
-			
-		} else 
-		if (fila == -1) {
-
+		switch (jugador) {
+			case JUGADOR   : printf("\nTurno: Jugador\n");  break;
+			case PC        : printf("\nTurno: Maquina\n");  break;
+			case JUGADOR_1 : printf("\nTurno: Jugador1\n"); break;
+			case JUGADOR_2 : printf("\nTurno: Jugador2\n");
 		}
 
-		printf("Columna: ");
-		scanf("%i", &columna);
-		getchar();
-
-		if (fila < 1 || columna < 1 || fila > dimension || columna > dimension) {
-			printf("\nCasilla invalida.\n");
+		// Manejamos la entrada dependiento del jugador
+		if (jugador == JUGADOR || jugador == JUGADOR_1 || jugador == JUGADOR_2) {
+			// Pedimos la casilla
+			printf("Fila: ");
+			scanf("%i", &fila);
 			getchar();
 
-			continue;
+			if (fila == 0) {
+				
+			} else 
+			if (fila == -1) {
+
+			}
+
+			printf("Columna: ");
+			scanf("%i", &columna);
+			getchar();
+
+			if (fila < 1 || columna < 1 || fila > dimension || columna > dimension) {
+				printf("\nCasilla invalida.\n");
+				getchar();
+
+				continue;
+			}
+		} else {
+			printf("Fila: ");
+			sleep(3);
+			fila = rand() % dimension + 1;
+			printf("%i\nColumna: ", fila);
+			sleep(3);
+			columna = rand() % dimension + 1;
+			printf("%i", columna);
+			sleep(3);
 		}
 
+		// Procesamos la lógica
 		// Comprobamos la casilla y sus vecinos y barremos de arriba a abajo
-		int casilla_y = fila - 1;
-		int casilla_x = columna - 1;
-		int numero = casillas[ casilla_y ][ casilla_x ];
-		int by;
+		huecos = 0;
+		casilla_y = fila - 1;
+		casilla_x = columna - 1;
+		numero = casillas[ casilla_y ][ casilla_x ];
+		by;
 		f = casilla_y + 1;
 		c = casilla_x + 1;
 
@@ -113,9 +146,33 @@ void mostrar_tablero (int tipo_juego) {
 
 				// Barremos la columna hacia abajo
 				for (by = y - 1; by >= -1; by--) {
-					casillas[ by + 1 ][ x ] = (by < 0) ? 0 : casillas[ by ][ x ] ;
+					if (by < 0) {
+						casillas[ by + 1 ][ x ] = 0;
+						huecos++;
+					} else {
+						casillas[ by + 1 ][ x ] = casillas[ by ][ x ];
+					}
 				}
 			}
+		}
+
+		if (jugador == JUGADOR || jugador == JUGADOR_1) {
+			puntaje1 += huecos;
+		} else {
+			puntaje2 += huecos;
+		}
+
+		// Comprobamos si la partida terminó
+		if (puntaje1 + puntaje2 == dimension * dimension) {
+			return;
+		}
+		
+		// Cambiamos de turno
+		switch (jugador) {
+			case JUGADOR   : jugador = PC;        ; break;
+			case PC        : jugador = JUGADOR;   ; break;
+			case JUGADOR_1 : jugador = JUGADOR_2; ; break;
+			case JUGADOR_2 : jugador = JUGADOR_1; ;
 		}
 	}
 }
